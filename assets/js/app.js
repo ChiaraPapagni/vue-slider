fbaefb8c5cb37b5d3fe4381aa8fcb7b35ddc0d6c
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-29 15:24:24 +0200

    fixed classe active

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,29 +30,22 @@ const app = new Vue({
             },
         ],
         c: 0,
-        isActive: true,
         timer: 0,
     },
     methods: {
         slideUp() {
-            this.isActive = false;
             if (this.c != 0) {
                 this.c--;
-                this.isActive = true;
             } else {
                 this.c = this.slides.length - 1;
-                this.isActive = true;
             }
             this.resetAutoplay();
         },
         slideDown() {
-            this.isActive[this.c] = false;
             if (this.c < (this.slides.length - 1)) {
                 this.c++;
-                this.isActive[this.c] = true;
             } else {
                 this.c = 0;
-                this.isActive[this.c] = true;
             }
             this.resetAutoplay();
         },
