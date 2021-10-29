ba91efecf6ec16264baa5d7cea3144852b505042
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-29 12:37:51 +0200

    creata struttura principale html

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,5 +29,6 @@ const app = new Vue({
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
             },
         ],
+        c: 0,
     },
 });
